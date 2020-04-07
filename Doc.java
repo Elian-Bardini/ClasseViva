cdf89aafc7908bc91d2422485dc23f67bc3f67a5
Federico Agazzi <fagazzi@studenti.iiissgadda.it>
2020-04-07 17:32:45 +0200

    Array di docenti

---
+++ Doc.java
@@ -0,0 +1,22 @@
+/*import java.util.Arrays;
+public class Doc {
+	public Docente[] docente;
+	static final int MAX_NUM=100;
+	
+	
+	public Doc() {
+		docente=new Docente[MAX_NUM];
+
+		for(int i=0; i < MAX_NUM; i++)
+			docente[i]=new Docente();
+		
+		}
+
+
+	@Override
+	public String toString() {
+		return "Doc [docente=" + Arrays.toString(docente) + "]";
+	}
+	
+}
+*/
