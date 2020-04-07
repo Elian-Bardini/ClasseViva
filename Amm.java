8ebe4c36470749e392f49e34e7711c06750038ca
Federico Agazzi <fagazzi@studenti.iiissgadda.it>
2020-04-07 17:30:22 +0200

    Creatazione classe Amm

---
+++ Amm.java
@@ -0,0 +1,22 @@
+/*import java.util.Arrays;
+
+public class Amm {
+	public Amministratore[] amministratore;
+	static final int MAX_NUM=100;
+
+public Amm() {
+	amministratore=new Amministratore[MAX_NUM];
+
+	for(int i=0; i < MAX_NUM; i++)
+		amministratore[i]=new Amministratore();
+	
+	}
+
+@Override
+public String toString() {
+	return "Amm amministratore=" + Arrays.toString(amministratore) + "";
+}
+
+
+}
+*/
