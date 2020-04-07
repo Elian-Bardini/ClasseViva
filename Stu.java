f68f6ff669e729635deeb6c310c86ff85cea4721
Federico Agazzi <fagazzi@studenti.iiissgadda.it>
2020-04-07 17:34:35 +0200

    Array di studenti

---
+++ Stu.java
@@ -0,0 +1,24 @@
+/*import java.util.Arrays;
+public class Stu {
+
+		public Studente[] studente;
+		static final int MAX_NUM=100;
+		
+		
+		public Stu() {
+			studente=new Studente[MAX_NUM];
+
+			for(int i=0; i < MAX_NUM; i++)
+				studente[i]=new Studente();
+			
+			}
+
+
+		@Override
+		public String toString() {
+			return "Stu [studente=" + Arrays.toString(studente) + "]";
+		}
+		
+
+}
+*/
