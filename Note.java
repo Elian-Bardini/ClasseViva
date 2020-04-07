88872b13895a04c7bba16958ff1e3327ce64e2a8
ElianBardini <ebardini@studenti.issgadda.it>
2020-04-07 16:24:05 +0200

    Creato classe note

--- Note.java
+++ Note.java
@@ -1,4 +1,24 @@
 
 public class Note {
+	private String descrizione;
 
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+	public Note(String descrizione) {
+		super();
+		this.descrizione = descrizione;
+	}
+
+	@Override
+	public String toString() {
+		return "Note descrizione=" + descrizione + "";
+	}
+	
+	
 }
